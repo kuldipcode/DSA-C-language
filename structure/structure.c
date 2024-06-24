d0e54a422f0767df80edd69a103936d29359f7ae
Kuldipkumar Prajapati <kuldip.code@gmail.com>
2024-06-24 11:35:23 +0530

    memory

---
+++ structure/structure.c
@@ -0,0 +1,20 @@
+//  https://www.w3schools.com/c/c_structs.php
+// unlike array structure has different data types
+#include <stdio.h>
+
+// Create a structure
+struct myStructure {
+  int myNum;
+  char myLetter;
+  char myString[30];
+};
+
+int main() {
+  // Create a structure variable and assign values to it
+  struct myStructure s1 = {13, 'B', "Some text"};
+
+  // Print values
+  printf("%d %c %s", s1.myNum, s1.myLetter, s1.myString);
+
+  return 0;
+}
