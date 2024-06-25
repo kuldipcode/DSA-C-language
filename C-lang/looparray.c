3348e5313308b9473e4a00a81d468aee24a01d9e
Kuldipkumar Prajapati <kuldip.code@gmail.com>
2024-06-25 11:49:13 +0530

    malloc aith array

--- C-lang/looparray.c
+++ C-lang/looparray.c
@@ -1,12 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
  
-int main() {
-  int myNumbers[4] = {25, 50, 75, 100};
-  int *ptr = myNumbers;
-  int i;
-
-  for (i = 0; i < 4; i++) {
-    printf("%d\n", *(ptr + i));
-  }
-  return 0;
+int main()
+{
+ 
+    // This pointer will hold the
+    // base address of the block created
+    int* ptr;
+    int n, i;
+ 
+    // Get the number of elements for the array
+    printf("Enter number of elements:");
+    scanf("%d",&n);
+    printf("Entered number of elements: %d\n", n);
+ 
+    // Dynamically allocate memory using malloc()
+    ptr = (int*)malloc(n * sizeof(int));
+ 
+    // Check if the memory has been successfully
+    // allocated by malloc or not
+    if (ptr == NULL) {
+        printf("Memory not allocated.\n");
+        exit(0);
+    }
+    else {
+ 
+        // Memory has been successfully allocated
+        printf("Memory successfully allocated using malloc.\n");
+ 
+        // Get the elements of the array
+        for (i = 0; i < n; ++i) {
+            ptr[i] = i + 1;
+        }
+ 
+        // Print the elements of the array
+        printf("The elements of the array are: ");
+        for (i = 0; i < n; ++i) {
+            printf("%d, ", ptr[i]);
+        }
+    }
+ 
+    return 0;
 }
